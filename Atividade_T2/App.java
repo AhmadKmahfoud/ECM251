490e6d1aa9b613599910288e1bd32932bec48e96
AhmadKmahfoud <82835417+AhmadKmahfoud@users.noreply.github.com>
2022-06-09 12:26:04 -0300

    Add files via upload
    Trabalho T2 murilo

---
+++ Atividade_T2/App.java
@@ -0,0 +1,13 @@
+/**
+ * Ahmad Kheder Mahfoud RA:20.01323-0
+ * prof murilo
+ * turma 2
+**/
+
+
+
+public class App {
+      public static void main(String[] args) throws Exception {
+        Sistema.run();
+    }
+}
